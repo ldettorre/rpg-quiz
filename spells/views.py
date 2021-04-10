bb4994bc55d1d47d0d4d3df4b3abcfbc3b248440
Luca D'Ettorre <ldettorre21@gmail.com>
2021-04-10 03:07:04 +0100

    I imported ranint and created a while loop so that while the length of random spells is less than 2, 2 unique random spells will be added to render on the index page

--- spells/views.py
+++ spells/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Spell
+from random import randint
 
 # Create your views here.
 
 def index(request):
-    spells = Spell.objects.all()
+    count = Spell.objects.count()
+    randomspells = []
+    while len(randomspells) < 2:
+        x = randint(1, count)
+        random_selection = get_object_or_404(Spell, id=x)
+        if random_selection not in randomspells:
+            randomspells.append(random_selection)
     context = {
-        'spells': spells,
+        'randomspells': randomspells
     }
+    
     return render(request, 'spells/index.html', context)
