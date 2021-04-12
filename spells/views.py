4cd2695e104d8d310b8024985aebe8bf12b79c37
Luca D'Ettorre <ldettorre21@gmail.com>
2021-04-12 04:05:39 +0100

    Created a dictionary within the session called 'total' that keeps record of user selections and updates the class types with the points value that each selection carries

--- spells/views.py
+++ spells/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Spell
 from random import randint, random
 import random
@@ -6,17 +6,28 @@ import random
 # Create your views here.
 
 def index(request, spell_id=None):
+    '''Generate two random spells for the user to choose from'''
     prev_selection = None
     if spell_id != None:
         prev_selection = get_object_or_404(Spell, id=spell_id)
+        print(prev_selection.class_type, prev_selection.points)
+
+        '''We called our record of submissions 'total' '''
+        total = request.session.get('total', {})
+
+        '''We update the class total by the number of points the user selection carries'''
+        total[prev_selection.class_type] = total.get(prev_selection.class_type, 0) + prev_selection.points
+
+        request.session['total'] = total
+        print(total)
+        
+
     randomspells = []
-  
     while len(randomspells) < 2:
         random_selection = random.choice(Spell.objects.all())
         print(random_selection)
-        # random_selection = get_object_or_404(Spell, id=x)
-        if random_selection not in randomspells:
-            if random_selection != prev_selection:
+        if random_selection != prev_selection:
+            if random_selection not in randomspells:
                 randomspells.append(random_selection)
     context = {
         'randomspells': randomspells,
@@ -24,4 +35,5 @@ def index(request, spell_id=None):
     }
     
     return render(request, 'spells/index.html', context)
+
 
