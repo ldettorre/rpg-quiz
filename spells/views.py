799f5a6ad2c3fe5d05cb767f3e2afcaa8993ced8
Luca D'Ettorre <ldettorre21@gmail.com>
2021-04-12 22:23:02 +0100

    Added a loop and if statement to check if any of the recorded class entries exceed a set int which is the threshold for when to stop. And when it's hit, a message is printed to say which class the user should be. This will be adapted later to render a new page delivering the message

--- spells/views.py
+++ spells/views.py
@@ -6,26 +6,32 @@ import random
 # Create your views here.
 
 def index(request, spell_id=None):
-    '''Generate two random spells for the user to choose from'''
+    '''Check if a spell has been selected'''
     prev_selection = None
     if spell_id != None:
         prev_selection = get_object_or_404(Spell, id=spell_id)
-        print(prev_selection.class_type, prev_selection.points)
+        # print(prev_selection.class_type, prev_selection.points)
 
-        '''We called our record of submissions 'total' '''
+        '''The name for our record of submissions is 'total' '''
         total = request.session.get('total', {})
 
         '''We update the class total by the number of points the user selection carries'''
         total[prev_selection.class_type] = total.get(prev_selection.class_type, 0) + prev_selection.points
 
         request.session['total'] = total
-        print(total)
-        
 
+        ''' Check if any class has exceeded the point limit'''
+        for t in total:
+            if total[t] >= 25:
+                print("You ARE A " + str(t))
+        print(total)
+
+        
+    '''Generate two random spells for the user to choose from'''
     randomspells = []
     while len(randomspells) < 2:
         random_selection = random.choice(Spell.objects.all())
-        print(random_selection)
+        # print(random_selection)
         if random_selection != prev_selection:
             if random_selection not in randomspells:
                 randomspells.append(random_selection)
