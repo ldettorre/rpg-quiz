1d49a429fd0301b97f885b867dc37bffb5bf48b9
Luca D'Ettorre <ldettorre21@gmail.com>
2021-04-14 02:18:05 +0100

    Updated the logic within the spells index so that previous selection is created as a session list. This stores all of the users previous entries and is then used to compare the id's with those that are randomly pulled from the spells db. If the id matches any of those within previous_selections, a new one is pulled until there are 2 that don't match the list or random spells list. There is also a check in place to determine if there aren't enough spells left. In which case the process stops and an 'error message' is rendered. Final note is that the core index view could do with refactoring which will be the next task

--- spells/views.py
+++ spells/views.py
@@ -8,9 +8,17 @@ import random
 def index(request, spell_id=None):
     '''Check if a spell has been selected'''
     prev_selection = None
+    previous_selections = []
     if spell_id != None:
         prev_selection = get_object_or_404(Spell, id=spell_id)
         # print(prev_selection.class_type, prev_selection.points)
+        previous_selections = request.session.get('previous_selections', [])
+        previous_selections.append(prev_selection.id)                                                    
+        print(previous_selections)
+
+        '''We create a previous selections list to avoid duplicates'''
+        request.session['previous_selections'] = previous_selections
+        # print(previous_selections)
 
         '''The name for our record of submissions is 'total' '''
         total = request.session.get('total', {})
@@ -22,8 +30,7 @@ def index(request, spell_id=None):
 
         ''' Check if any class_type has exceeded the point limit'''
         for t in total:
-            if total[t] >= 25:
-                # return redirect('result')
+            if total[t] >= 20:
                 result_message = "Congratulations! You're ready to discover your class type."
                 class_type = t
                 context = {
@@ -32,19 +39,30 @@ def index(request, spell_id=None):
                 }
                 return render(request, 'spells/index.html', context)
 
-        
-    '''Generate two random spells for the user to choose from'''
-    randomspells = []
-    while len(randomspells) < 2:
-        random_selection = random.choice(Spell.objects.all())
-        if random_selection != prev_selection:
-            if random_selection not in randomspells:
-                randomspells.append(random_selection)
+    '''Check if there are enough spells left to generate 2 at random'''
+    if len(previous_selections) <= len(Spell.objects.all())-2:
+
+        '''Generate said random spells'''
+        randomspells = []
+        while len(randomspells) < 2:
+            random_selection = random.choice(Spell.objects.all())
+            print(random_selection.id)
+            if random_selection.id not in previous_selections:
+                if random_selection not in randomspells:
+                    randomspells.append(random_selection)
+    else:
+        error_message = "You finished the quiz and still don't have a destiny?!"
+        context = {
+                    'error_message': error_message,
+                }
+        return render(request, 'spells/index.html', context)
+
+
+
     context = {
         'randomspells': randomspells,
         'prev_selection': prev_selection,
     }
-    
     return render(request, 'spells/index.html', context)
 
 
