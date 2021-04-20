49192f13da246dad421d759c89ff447c9eee28dd
Luca D'Ettorre <ldettorre21@gmail.com>
2021-04-20 04:37:36 +0100

    While trying to save all the spells from the dungeons and dragons api to my project, I was running into attribute errors and relative path errors. So I decided to try and create the classes first as it was an easier batch of data to work with. This worked as hoped and I can now begin to work on the spells again with a better understanding.  A similar loop will be used but classes will need to be matched as they are a many to many field

--- spells/api.py
+++ spells/api.py
@@ -1,33 +1,56 @@
 import requests
 import json
+from .models import ClassType
+
+# base_url = 'https://www.dnd5eapi.co'
+
+# response = requests.get(base_url + '/api/spells/')
+
+# data = response.json()
+
+# def spell_entry(obj):
+#     data = obj
+#     count = 0
+#     for i in data['results']:
+#         spell = requests.get(base_url + i['url'])
+#         name = spell.json()['name']
+#         classes = spell.json()['classes']
+#         for i in classes:
+#             class_type = i['name']
+#             print(class_type)
+#             existing_classes = ClassType.objects.all()
+#             for e in existing_classes:
+#                 # if class_type == existing_classes[e]:
+#                 print(existing_classes[e])
+#         level = spell.json()['level']
+#         comical_description = spell.json()['desc']
+#         fantasy_universe = "D&D"
+#         count += 1
+#         if count >=5:
+#             break
+        
+        # spell = Spell.objects.create(name=name, 
+        #     class_type=class_type, 
+        #     level=level, 
+        #     points=level, 
+        #     comical_description=comical_description, 
+        #     fantasy_universe=fantasy_universe)
+        # spell.save()
+    
+
+    
+# spell_entry(data)
+
 
 base_url = 'https://www.dnd5eapi.co'
 
-response = requests.get(base_url + '/api/spells/')
+response = requests.get(base_url + '/api/classes/')
 
 data = response.json()
 
-test_response = requests.get('https://www.dnd5eapi.co/api/spells/acid-splash')
-test_data = test_response.json()
-
-def spell_entry(obj):
-    data = obj
-    count = 0
-    for i in data['results']:
-        spell = requests.get(base_url + i['url'])
-        print(spell.json()['name'])
-        classes = spell.json()['classes']
-        for i in classes:
-            print(i['name'])
-        print(spell.json()['level'])
-        print(spell.json()['desc'])
-        count += 1
-        if count >=5:
-            break
-    
-spell_entry(data)
-
-
+for d in data['results']:
+    new_class = d['name']
+    ClassType.objects.create(name=new_class)
 
 
 
