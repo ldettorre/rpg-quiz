24bc45fe7e63344b44153053d5c15196af97c142
Luca D'Ettorre <ldettorre21@gmail.com>
2021-04-20 23:03:29 +0100

    I began breaking down the old spell_entry function to compare the types of values I was getting back fron the api and db. This better allows me to see when I can make the comparison before attempting to assign the api class to the new object

--- spells/api.py
+++ spells/api.py
@@ -2,32 +2,33 @@ import requests
 import json
 from .models import ClassType
 
-# base_url = 'https://www.dnd5eapi.co'
 
-# response = requests.get(base_url + '/api/spells/')
+base_url = 'https://www.dnd5eapi.co'
 
-# data = response.json()
+response = requests.get(base_url + '/api/spells/')
 
-# def spell_entry(obj):
-#     data = obj
-#     count = 0
-#     for i in data['results']:
-#         spell = requests.get(base_url + i['url'])
-#         name = spell.json()['name']
-#         classes = spell.json()['classes']
-#         for i in classes:
-#             class_type = i['name']
-#             print(class_type)
-#             existing_classes = ClassType.objects.all()
-#             for e in existing_classes:
-#                 # if class_type == existing_classes[e]:
-#                 print(existing_classes[e])
-#         level = spell.json()['level']
-#         comical_description = spell.json()['desc']
-#         fantasy_universe = "D&D"
-#         count += 1
-#         if count >=5:
-#             break
+data = response.json()
+
+def spell_entry(obj):
+    data = obj
+    count = 0
+    for i in data['results']:
+        spell = requests.get(base_url + i['url'])
+        # name = spell.json()['name']
+        classes = spell.json()['classes']
+        for i in classes:
+            class_type = i['name']
+            print(class_type, type(class_type))
+            existing_classes = ClassType.objects.all()
+            for e in existing_classes:
+                if class_type == e.name:
+                    print("Match")
+        # level = spell.json()['level']
+        # comical_description = spell.json()['desc']
+        # fantasy_universe = "D&D"
+        count += 1
+        if count >=5:
+            break
         
         # spell = Spell.objects.create(name=name, 
         #     class_type=class_type, 
@@ -35,22 +36,22 @@ from .models import ClassType
         #     points=level, 
         #     comical_description=comical_description, 
         #     fantasy_universe=fantasy_universe)
-        # spell.save()
     
 
     
-# spell_entry(data)
+spell_entry(data)
 
+# Below loops over the API classes and creates them within our db.
 
-base_url = 'https://www.dnd5eapi.co'
+# base_url = 'https://www.dnd5eapi.co'
 
-response = requests.get(base_url + '/api/classes/')
+# response = requests.get(base_url + '/api/classes/')
 
-data = response.json()
+# data = response.json()
 
-for d in data['results']:
-    new_class = d['name']
-    ClassType.objects.create(name=new_class)
+# for d in data['results']:
+#     new_class = d['name']
+#     ClassType.objects.create(name=new_class)
 
 
 
