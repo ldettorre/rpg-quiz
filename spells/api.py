e12eab533975a89c312d23b410cbf6964386aa39
Luca D'Ettorre <ldettorre21@gmail.com>
2021-04-23 22:11:27 +0100

    I created a feature entry function based off of the spell entry. However with some features not having a level key, I used the try/except feature so attempt to find a level and if it fails, assign a default of 0. Features then successfully populated the db and were editable within the django admin dashboard

--- spells/api.py
+++ spells/api.py
@@ -5,9 +5,16 @@ from .models import ClassType, Spell
 
 base_url = 'https://www.dnd5eapi.co'
 
-
+''' Class API Resource '''
 # response = requests.get(base_url + '/api/classes/')
-# data = response.json()
+
+''' Spells API Resource '''
+# response = requests.get(base_url + '/api/spells/')
+
+''' Features API Resource '''
+response = requests.get(base_url + '/api/features/')
+
+data = response.json()
 
 def class_entry(obj):
     '''Below loops over the API classes and creates them within our db.'''
@@ -15,9 +22,6 @@ def class_entry(obj):
         new_class = d['name']
         ClassType.objects.create(name=new_class)
 
-
-# response = requests.get(base_url + '/api/spells/')
-# data = response.json()
 
 def spell_entry(obj):
     ''' Loop over each spell for it's url, then use the url in a request to access
@@ -43,7 +47,8 @@ def spell_entry(obj):
         spell_desc = spell.json()['desc']
         spell_universe = "D&D"
     
-        spell = Spell.objects.create(name=spell_name, 
+        spell = Spell.objects.create(
+            name=spell_name, 
             level=spell_level, 
             points=spell_level, 
             comical_description=spell_desc, 
@@ -52,14 +57,41 @@ def spell_entry(obj):
             spell.class_type.add(i)
     
 
-    
+
+def feature_entry(obj):
+    ''' Loop over every feature within the feature resource for its url. Then
+    use the url to pull the neccesary data and create a new object. Features
+    with no levels are set as 0 by default thanks to the try/except for KeyErrors'''
+
+    features = obj
+    for i in data['results']:
+        feature = requests.get(base_url + i['url'])
+        feature_name = feature.json()['name']
+        feature_class = feature.json()['class']['name']
+        new_classes = []
+
+        existing_classes = ClassType.objects.all()
+        for e in existing_classes:
+            if feature_class == e.name:
+                new_classes.append(e.id)
+        try:
+            feature_level = feature.json()['level']
+        except KeyError:
+            print("This feature has no level. Default will be set to 0")
+            feature_level = 0
+        feature_desc = feature.json()['desc']  
+        feature_universe = "D&D"
+        feature_ability = "Feature"
+
+        spell = Spell.objects.create(
+            name=feature_name,
+            level=feature_level, 
+            points=feature_level, 
+            comical_description=feature_desc,
+            ability_type=feature_ability,
+            fantasy_universe=feature_universe)
+        for i in new_classes:
+            spell.class_type.add(i)
 
 
-
-
-
-
-
-
-
-
+feature_entry(data)
