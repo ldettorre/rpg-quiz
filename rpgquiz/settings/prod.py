d4a506bf9d7bad27f922e23e5b39db3a9c4409bf
Luca D'Ettorre <ldettorre21@gmail.com>
2021-06-01 01:38:52 +0100

    Removed dj database url from line 8 in prod.py as I want to see if the config alone will allow Heroku to use the config var to connect to postgresql

--- rpgquiz/settings/prod.py
+++ rpgquiz/settings/prod.py
@@ -5,5 +5,5 @@ from .base import *
 SECRET_KEY = config("SECRET_KEY")
 
 DATABASES= {
-        'default': dj_database_url.parse(config('DATABASE_URL'))
+        'default': config('DATABASE_URL')
     }
