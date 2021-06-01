0ea3923cf716c9afe03f2cf25d1e3195559f280d
Luca D'Ettorre <ldettorre21@gmail.com>
2021-06-01 01:55:28 +0100

    DEPLOYMENT: Changed line 8 of prod.py back to include dj_database_url and also imported the package into the file

--- rpgquiz/settings/prod.py
+++ rpgquiz/settings/prod.py
@@ -1,9 +1,9 @@
 from .base import * 
-
+import dj_database_url
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 SECRET_KEY = config("SECRET_KEY")
 
 DATABASES= {
-        'default': config('DATABASE_URL')
+        'default': dj_database_url.parse(config('DATABASE_URL'))
     }
